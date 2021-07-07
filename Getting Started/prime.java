bea3b517367ea7d3ec0ba65a1293757246b6007b
Madhav <aroraraghav008@gmail.com>
2021-07-08 02:16:13 +0530

    'Getting_Started'

---
+++ Getting Started/prime.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class prime {
+    
+    public static void prime_no(int n){
+        boolean flag = true;
+        for(int i=2;i*i<=n;i++){
+            if(n % i == 0)
+                flag = false;
+        }
+        if(!flag)
+            System.out.println("not prime");
+        else 
+            System.out.println("prime");
+    }
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i=0;i<t;i++){
+            int n = sc.nextInt();
+            prime_no(n);
+        }
+    }     
+}

9a8c7fdd77aaea6fdb4f0cb8cf21009d25aa5b5a
Madhav <aroraraghav008@gmail.com>
2021-07-07 23:10:33 +0530

    update

---
+++ Getting Started/Print_Z.java
@@ -0,0 +1,10 @@
+public class Print_Z{
+
+    public static void main(String [] args){
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}

c486fbbd03d72406fd0800bb4b62a8ff6cf36ad6
Madhav <aroraraghav008@gmail.com>
2021-07-08 20:39:03 +0530

    'update'

--- Basics of Programming/Patterns/pattern_12.java
+++ Basics of Programming/Patterns/pattern_12.java
@@ -1,18 +1,23 @@
 import java.util.*;
 
-public class pattern_11 {
+public class pattern_12 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        
+
         int n = scn.nextInt();
-        int count = 1;
+        int a = 0;
+        int b = 1;
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                    System.out.print(count+"	");
-                    count++;
+                System.out.print(a + "	");
+                int c=a+b;
+                a=b;
+                b=c;
             }
             System.out.println();
         }
+
     }
 }

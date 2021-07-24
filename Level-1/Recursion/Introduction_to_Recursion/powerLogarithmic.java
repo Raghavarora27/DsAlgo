2c2bed8d0ff9446a3a2b5089fb3c77852110da18
Madhav <aroraraghav008@gmail.com>
2021-07-24 17:38:41 +0530

    update

---
+++ Level-1/Recursion/Introduction_to_Recursion/powerLogarithmic.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class powerLogarithmic {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(power(a,b));
+    }
+
+    public static int power(int x, int n){
+        if(n==0)
+        return 1;
+        
+        int ans = power(x,n/2);
+        ans *= ans;
+        return n % 2 == 0 ? ans : ans*x;
+    }
+
+}

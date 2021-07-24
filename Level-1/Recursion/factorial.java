981975d92e72cfdd092752f9c89bf9d33cc49833
Madhav <aroraraghav008@gmail.com>
2021-07-24 17:28:41 +0530

    update

---
+++ Level-1/Recursion/factorial.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class factorial {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Factorial(n));
+    }
+
+    public static int Factorial(int n){
+        if(n <= 1)
+            return 1;
+        return n * Factorial(n-1); 
+    }
+
+}

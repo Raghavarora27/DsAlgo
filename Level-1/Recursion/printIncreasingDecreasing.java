8daaace979634c638a5fbfc8d9d6218b1503720e
Madhav <aroraraghav008@gmail.com>
2021-07-24 17:26:36 +0530

    update

---
+++ Level-1/Recursion/printIncreasingDecreasing.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class printIncreasingDecreasing {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printincreasingdecreasing(n);
+    }
+
+    public static void printincreasingdecreasing(int n){
+        if(n < 1)
+            return;
+        
+        System.out.println(n);
+        printincreasingdecreasing(n-1);
+        System.out.println(n);
+    }
+
+}

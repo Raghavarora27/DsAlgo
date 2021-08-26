565be635e21d89bdb7be28ede875e2cf968ba43d
Madhav <aroraraghav008@gmail.com>
2021-08-26 08:30:52 +0530

    update

--- Level-1/Recursion/Recursion_WayUp/Print_Subsequence.java
+++ Level-1/Recursion/Recursion_WayUp/Print_Subsequence.java
@@ -1,12 +1,15 @@
 import java.io.*;
 import java.util.*;
 
+// Void Type Recursion
+// upar jate hue answer create karke print kar rhe h
 public class Print_Subsequence {
 
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         String str = sc.nextLine();
         printSS(str,"");
+        // System.out.println(printSS1(str,""));
     }
 
     public static void printSS(String str, String ans) {
@@ -21,6 +24,24 @@ public class Print_Subsequence {
         
         printSS(code,ans + ch);
         printSS(code,ans);
+    }
+    
+
+    // In this we are returning the number of answers made while going up in recursion
+    public static int printSS1(String str, String ans) {
+        if(str.length() == 0){
+            System.out.print(ans);
+            System.out.println();
+            return 1;
+        }
+        
+        char ch = str.charAt(0);
+        String code = str.substring(1);
+        int count = 0;
+
+        count += printSS1(code,ans + ch);
+        count += printSS1(code,ans);
+        return count;
     }
 
 }

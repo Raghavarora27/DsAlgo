e687fdcd7bb1702f55a79b0d4dc76557377f6b91
Madhav <aroraraghav008@gmail.com>
2021-09-05 12:32:22 +0530

    update

---
+++ Level-1/Time Complexity/PartitionOnAnArray.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class PartitionOnAnArray {
+
+  public static void partition(int[] arr, int data){
+    int  p = -1,itr = 0,n = arr.length;
+    while(itr < n){
+        if(arr[itr] <= data)
+            swap(arr,itr,++p);
+        itr++;
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    int pivot = scn.nextInt();
+    partition(arr,pivot);
+    print(arr);
+  }
+
+}

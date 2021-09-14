34c816580349e1a25f619a75f0b08f4857aec502
Raghavarora27 <aroraraghav008@gmail.com>
2021-09-14 09:17:41 +0530

    update

---
+++ Level-1/Linked List/questions/Leetcode_83.java
@@ -0,0 +1,47 @@
+package questions;
+
+public class Leetcode_83 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null || head.next == null)
+                return head;
+            ListNode dummy = new ListNode(-101); // see constraints
+            ListNode dp = dummy;
+
+            ListNode curr = head;
+
+            while (curr != null) {
+                while (curr != null && dp.val == curr.val) {
+                    ListNode forw = curr.next;
+                    curr.next = null;
+                    curr = forw;
+                }
+
+                dp.next = curr;
+                if (curr != null) {
+                    curr = curr.next;
+                    dp = dp.next;
+                }
+            }
+
+            return dummy.next;
+        }
+    }
+}

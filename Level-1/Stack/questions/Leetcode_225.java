1afd784abd2ab9786a5c359f5813c45c0ee44416
Raghavarora27 <aroraraghav008@gmail.com>
2021-09-21 17:57:08 +0530

    update

--- Level-1/Stack/questions/Leetcode_225.java
+++ Level-1/Stack/questions/Leetcode_225.java
@@ -37,4 +37,42 @@ public class Leetcode_225 {
         }
     }
 
+    // pop Efficient
+    class MyStack {
+
+        LinkedList<Integer> que = new LinkedList();
+        LinkedList<Integer> temp = new LinkedList();
+
+        public MyStack() {
+
+        }
+
+        public void push(int x) {
+            temp.addLast(x);
+            transfer(que, temp);
+
+            LinkedList<Integer> temp1 = que;
+            que = temp;
+            temp = temp1;
+        }
+
+        private void transfer(LinkedList<Integer> que1, LinkedList<Integer> que2) {
+            while (que1.size() != 0) {
+                que2.addLast(que1.removeFirst());
+            }
+        }
+
+        public int pop() {
+            return que.removeFirst();
+        }
+
+        public int top() {
+            return que.getFirst();
+        }
+
+        public boolean empty() {
+            return que.size() == 0;
+        }
+    }
+
 }

e22cac760635d3658595fcec8a0deae5d17409f2
Raghavarora27 <aroraraghav008@gmail.com>
2021-09-21 17:27:10 +0530

    update

--- Level-1/Stack/questions/Leetcode_232.java
+++ Level-1/Stack/questions/Leetcode_232.java
@@ -42,4 +42,40 @@ public class Leetcode_232 {
         }
     }
 
+    // Pop Efficient
+    class MyQueue {
+
+        LinkedList<Integer> st = new LinkedList<>();
+        LinkedList<Integer> temp = new LinkedList<>();
+
+        public MyQueue() {
+
+        }
+
+        // O(n)
+        public void push(int x) {
+            transfer(st, temp);
+            st.addFirst(x);
+            transfer(temp, st);
+        }
+
+        private void transfer(LinkedList<Integer> st1, LinkedList<Integer> st2) {
+            while (st1.size() != 0)
+                st2.addFirst(st1.removeFirst());
+        }
+
+        // O(1)
+        public int pop() {
+            return st.removeFirst();
+        }
+
+        public int peek() {
+            return st.getFirst();
+        }
+
+        public boolean empty() {
+            return st.size() == 0;
+        }
+    }
+
 }

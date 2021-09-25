12e615cfdc6f8cd2849c264d426a204b006151bc
Raghavarora27 <aroraraghav008@gmail.com>
2021-09-25 07:31:19 +0530

    update

---
+++ Level-1/Stack/Next_greater.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Next_greater{
+
+    public static void nextGreater(int [] arr){
+        int n = arr.length;
+        int [] ans = new int[n];
+        Arrays.fill(ans, -1);
+        LinkedList<Integer> st = new LinkedList<>();
+
+        for(int i = 0; i < n ; i++){
+            while(st.size() != 0 && arr[st.getFirst()] < arr[i])
+                ans[st.removeFirst()] = arr[i];
+            
+            st.push(i);
+        }
+
+        for(int ele : ans)
+            System.out.println(ele);
+    }
+
+    public static void main(String [] args){
+        
+    }
+}

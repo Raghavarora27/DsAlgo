80dfdaaaaa4ca12f7fa0147418cea773d367cf2e
Raghavarora27 <aroraraghav008@gmail.com>
2021-10-03 14:38:44 +0530

    update

---
+++ Level-1/Binary Tree/BST.java
@@ -0,0 +1,56 @@
+import javax.xml.crypto.Data;
+
+public class BST {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        Node(int data) {
+            this(data, null, null); // Constructor Chinning
+        }
+    }
+
+    // T : O(n), S : O(1)
+    public static int size(Node node){
+        return node == null ? 0 : size(node.left) + size(node.right) + 1;
+    }
+    
+    // T : O(logn), S : O(1)  
+    public static int height(Node node){
+        return node == null ? -1 : Math.max(height(node.left), height(node.right)) + 1; 
+    }
+
+    // T : O(logn), S : O(1)    
+    public static int maximum(Node node){
+        while(node.right != null)
+            node = node.right;
+        return node.data;
+    }
+
+    // T : O(logn), S : O(1)
+    public static int minimum(Node node){
+        while(node.left != null)
+            node = node.left;
+        return node.data;
+    }
+
+    // T : O(logn), S : O(1)
+    public static boolean find(Node node,int data){
+        while(node != null){
+            if(node.data == data)
+                return true;
+            else if(node.data < data)
+                node = node.right;
+            else
+                node = node.left;
+        }
+        return false;
+    }
+}

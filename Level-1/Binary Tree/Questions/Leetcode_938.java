b746919c8e6e9eb61030f5d6bca791c2c9ce6ddf
Raghavarora27 <aroraraghav008@gmail.com>
2021-10-03 18:07:52 +0530

    update

--- Level-1/Binary Tree/Questions/Leetcode_938.java
+++ Level-1/Binary Tree/Questions/Leetcode_938.java
@@ -20,7 +20,7 @@ public class Leetcode_938 {
         }
     }
 
-    class Solution {
+    class Solution1 {
         public int rangeSumBST(TreeNode root, int low, int high) {
             pir(root, low, high);
             return sum;
@@ -40,4 +40,18 @@ public class Leetcode_938 {
             pir(root.right, low, high);
         }
     }
+
+    class Solution2 {
+        public int rangeSumBST(TreeNode root, int low, int high) {
+           if(root == null) return 0;
+           
+           if(root.val<low) return rangeSumBST(root.right,low,high);
+           
+           if(root.val>=low && root.val<=high) 
+               return rangeSumBST(root.left,low,high)+root.val+rangeSumBST(root.right,low,high);
+           
+           if(root.val>high) return rangeSumBST(root.left,low,high);
+           return 0;
+       }
+   }
 }

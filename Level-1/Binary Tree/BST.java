5d60462724e7b47bb3a974708eadd38002619d0f
Raghavarora27 <aroraraghav008@gmail.com>
2021-10-03 16:58:36 +0530

    update

--- Level-1/Binary Tree/BST.java
+++ Level-1/Binary Tree/BST.java
@@ -80,4 +80,19 @@ public class BST {
         Collections.reverse(list);
         return list;  
     }
+
+    public static int lca(Node node,int d1,int d2){
+        int lca = -1;
+        while(node != null){
+            if(node.data > d1 && node.data > d2)
+                node = node.left;
+            else if(node.data < d1 && node.data < d2)
+                node = node.right;
+            else{
+                lca = node.data;
+                break;
+            }
+        }
+        return lca;
+    }
 }

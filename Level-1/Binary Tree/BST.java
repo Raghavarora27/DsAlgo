7786d6fd51e65f1109f0aa5a88feabb0efed23be
Raghavarora27 <aroraraghav008@gmail.com>
2021-10-04 13:16:30 +0530

    update

--- Level-1/Binary Tree/BST.java
+++ Level-1/Binary Tree/BST.java
@@ -110,4 +110,13 @@ public class BST {
 
     }
 
+    public static Node addData(Node node,int data){
+        if(node == null)    return new Node(data);
+
+        if(node.data < data)
+            node.right = addData(node.right, data);
+        else if(node.data > data)
+            node.left = addData(node.left, data);
+        return node;
+    }
 }

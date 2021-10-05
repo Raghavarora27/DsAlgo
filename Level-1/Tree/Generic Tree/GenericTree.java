39ff6c640d600605bad22be02ee01f899ac395ed
Raghavarora27 <aroraraghav008@gmail.com>
2021-10-05 10:25:30 +0530

    update

--- Level-1/Tree/Generic Tree/GenericTree.java
+++ Level-1/Tree/Generic Tree/GenericTree.java
@@ -1,22 +1,30 @@
 import java.util.ArrayList;
 
 public class GenericTree {
-    public class Node{
+    public class Node {
         int data = 0;
         ArrayList<Node> childs;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.childs = new ArrayList<>();
         }
     }
-    
-    public static int size(Node node){
+
+    public static int size(Node node) {
         int count = 0;
-        
-        for(Node child : node.childs)
+
+        for (Node child : node.childs)
             count += size(child);
-        
+
         return count + 1;
-      }
+    }
+
+    public int height(Node root) {
+        int h = 0;
+        for (Node child : root.children)
+            h = Math.max(height(child) + 1, h);
+
+        return h;
+    }
 }
---
+++ Level-1/Tree/Generic Tree/Leetcode_559.java
@@ -0,0 +1,32 @@
+package GenericTree;
+
+public class Leetcode_559 {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    class Solution {
+        public int maxDepth(Node root) {
+            if (root == null)
+                return 0;
+            int h = 0;
+            for (Node child : root.children)
+                h = Math.max(maxDepth(child), h);
+
+            return h + 1;
+        }
+    }
+}

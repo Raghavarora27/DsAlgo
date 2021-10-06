0b25057f1a36c73b5d82887411566b0360dbd19e
Raghavarora27 <aroraraghav008@gmail.com>
2021-10-06 18:05:23 +0530

    update

--- Level-1/Tree/Generic Tree/GenericTree.java
+++ Level-1/Tree/Generic Tree/GenericTree.java
@@ -239,4 +239,23 @@ public class GenericTree {
         }
         return ub;
     }
+
+    // Linearize
+    public static Node getTail(Node node){
+        for(Node child : node.childs)
+            node = node.childs.get(0);
+        return node;
+    }
+
+    public static void linearize(Node node){
+        for(Node child : node.childs)
+            linearize(child);
+        
+        for(int i = node.childs.size()-1;i>0;i--){
+            Node tail = getTail(node.childs.get(i-1));
+            tail.childs.add(node.childs.get(i));
+
+            node.childs.remove(i);
+        }
+    }
 }

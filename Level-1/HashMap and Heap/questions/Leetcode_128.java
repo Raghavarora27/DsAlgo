b0c01df675afa86642af419545c7dff5726171d0
Raghavarora27 <aroraraghav008@gmail.com>
2021-10-18 09:00:39 +0530

    update

---
+++ Level-1/HashMap and Heap/questions/Leetcode_128.java
@@ -0,0 +1,35 @@
+package questions;
+
+import java.util.*;
+
+public class Leetcode_128 {
+    class Solution {
+        public int longestConsecutive(int[] arr) {
+            HashSet<Integer> set = new HashSet<>();
+    
+            for (int ele : arr) {
+                set.add(ele);
+            }
+    
+            int len = 0;
+            for (int ele : arr) {
+                if (!set.contains(ele))
+                    continue;
+    
+                int left = ele - 1, right = ele + 1;
+                set.remove(ele);
+    
+                while (set.contains(left))
+                    set.remove(left--);
+    
+                while (set.contains(right))
+                    set.remove(right++);
+    
+                if (right - left - 1 > len) {
+                    len = right - left - 1;
+                }
+            }
+            return len;
+        }
+    }
+}

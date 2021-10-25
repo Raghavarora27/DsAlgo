48022cd103cc87c5e732ec06a60a01618b14a811
Madhav2008 <aroramadhav65@gmail.com>
2021-10-25 11:19:53 +0530

    update

--- Level-1/Basics of Programming/Arrays & Number System/Subarrays.java
+++ Level-1/Basics of Programming/Arrays & Number System/Subarrays.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
-public class Subarrays{
+public class Subarrays {
 
-    public static void print_range(int [] arr,int si,int ei){
-        while(si <= ei){
+    public static void print_range(int[] arr, int si, int ei) {
+        while (si <= ei) {
             System.out.print(arr[si] + "    ");
             si++;
         }
         System.out.println();
     }
 
-    public static void subarray(int [] arr){
-        
-        for(int i=0;i<arr.length;i++){
-            for(int j=i;j<arr.length;j++){
+    public static void subarray(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
                 print_range(arr, i, j);
             }
         }
@@ -21,11 +21,11 @@ public class Subarrays{
 
     public static Scanner sc = new Scanner(System.in);
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         int n = sc.nextInt();
-        int [] arr = new int[n];
+        int[] arr = new int[n];
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 

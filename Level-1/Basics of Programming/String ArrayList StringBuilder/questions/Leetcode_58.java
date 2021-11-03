6c098d5d8abbc6ab37c0a958614260653cc1d384
Madhav2008 <aroramadhav65@gmail.com>
2021-11-04 00:13:00 +0530

    update

---
+++ Level-1/Basics of Programming/String ArrayList StringBuilder/questions/Leetcode_58.java
@@ -0,0 +1,25 @@
+package questions;
+
+public class Leetcode_58 {
+    class Solution {
+        public int lengthOfLastWord(String str) {
+            int n = str.length() - 1;
+
+            while (str.charAt(n) == ' ') {
+                n--;
+            }
+
+            int count = 0;
+            while (str.charAt(n) != ' ') {
+                count++;
+
+                if (n - 1 < 0)
+                    break;
+
+                n--;
+            }
+
+            return count;
+        }
+    }
+}
